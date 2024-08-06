2dc26ad12e4f72bea114647cc657e88ff1eb6d9b
jose <impacevanend@gmail.com>
2024-08-06 16:49:57 -0500

    se agrega estructura funcional

---
+++ app/Http/Controllers/TodosController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Todo;
+
+class TodosController extends Controller
+{
+    /**
+     * index para mostrar todos las tareas
+     * store para guardar una tarea
+     * update para actualizar una tarea
+     * destroy para eliminar una tarea
+     * edit para mostrar el formulario de edición.
+     */
+
+     public function store(Request $request){
+        $request->validate([
+            'title' => 'required|min:3'
+        ]);
+        $todo = new Todo();
+        $todo->title = $request->title;
+        $todo->save();
+
+        return redirect()->route('todos')->with('success', 'Tarea creada correctamente');
+     }
+
+     public function index(){
+        $todos = Todo::all();
+
+        return view('todos.index', ['todos' => $todos]);
+    }
+}
